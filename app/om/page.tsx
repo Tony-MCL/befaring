6e7488e7f8e59c0e150bdd564547faf24b31f4c1
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-06 10:51:50 +0100

    Create page.tsx

---
+++ app/om/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+return (
+<section className="card">
+<h1>Om</h1>
+<p>Bygget med Next.js (App Router) og statisk eksport. Data lagres i Firebase (Auth, Firestore, Storage).</p>
+</section>
+)
+}
