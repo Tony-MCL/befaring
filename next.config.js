f335d3482864eb030757437e895488fff8f9a8c2
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-06 10:49:18 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const isGhPages = !!process.env.NEXT_PUBLIC_BASE_PATH;
+
+
+module.exports = {
+output: 'export',
+trailingSlash: true,
+images: { unoptimized: true },
+basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+eslint: { ignoreDuringBuilds: true },
+typescript: { ignoreBuildErrors: false }
+};
