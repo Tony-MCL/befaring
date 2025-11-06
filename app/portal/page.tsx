5701760f0847f6345e3ea1fa5194fca842755b20
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-06 14:56:54 +0100

    Create page.tsx

---
+++ app/portal/page.tsx
@@ -0,0 +1,8 @@
+export default function PortalGate() {
+  return (
+    <article className="card">
+      <h1 style={{ marginTop: 0 }}>Portal</h1>
+      <p className="small">Tom gate for innlogging/roller senere.</p>
+    </article>
+  );
+}
